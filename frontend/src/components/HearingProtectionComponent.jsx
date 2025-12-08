38055d71314cf4fd39424580f9add734386b307f
Mithushan-MN <mithushan0099@gmail.com>
2025-12-08 16:58:03 +0530

    fixed american image

--- frontend/src/components/HearingProtectionComponent.jsx
+++ frontend/src/components/HearingProtectionComponent.jsx
@@ -1,5 +1,5 @@
-import hearingprotectionbgDesktop from "../assets/hearingProtection2.jpg";           // Your current wide image
-import hearingprotectionbgMobile from "../assets/hearingProtection-mobile.jpg";     // Create this
+import hearingprotectionbgDesktop from "../assets/hearingProtection2.png";           // Your current wide image
+import hearingprotectionbgMobile from "../assets/hearingProtection-mobile.png";     // Create this
 
 import { Link } from 'react-router-dom';
 
